0d19efa68f14ebdd75711befe753febae0d0183d
Lokoschade <fr.ratteit@gmx.de>
2024-05-30 17:24:15 +0200

    Language Komponente effizienter

--- src/components/language/Language.jsx
+++ src/components/language/Language.jsx
@@ -1,29 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import i18n from "../../i18n";
 import german from "../../assets/german flag.svg";
 import english from "../../assets/uk flag.svg";
 
 const Language = () => {
-  const [selectedLanguage, setSelectedLanguage] = useState(i18n.language); // i18n.language contains the language assigned to lng in i18n.js file.
-
   const chooseLanguage = async (e) => {
     e.preventDefault();
     const language = e.currentTarget.dataset.language;
     console.log("Changing language to:", language);
-    await i18n.changeLanguage(language); // Ensure the language is changed before updating state
-    setSelectedLanguage(language);
+    await i18n.changeLanguage(language);
   };
-
-  useEffect(() => {
-    const handleLanguageChange = (lng) => {
-      setSelectedLanguage(lng);
-    };
-    i18n.on("languageChanged", handleLanguageChange);
-
-    return () => {
-      i18n.off("languageChanged", handleLanguageChange);
-    };
-  }, []);
 
   return (
     <div>
