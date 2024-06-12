cf47e53952a624941e2613bea6e06bcc3ba0e256
Lokoschade <fr.ratteit@gmx.de>
2024-06-12 19:21:00 +0200

    rückgängig

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Navbar, Vorstellung, Problem, Ziele, Stimmen, Footer, Break, Impressum, ImpHeader, Datenschutz, DatenHeader, Wahl, WahlHeader } from './components';
-import { Route, Routes, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import './App.css';
 
 const App = () => {
@@ -8,22 +8,18 @@ const App = () => {
     <div className='App'>
       <div className='gradient__bg'>
         <Navbar />
-          <Switch>
-            <Routes>
+          <Routes>
               <Route path="" element={<Vorstellung />}/>
               <Route path="/datenschutz" element={<DatenHeader/>}/>
               <Route path="/impressum" element={<ImpHeader/>}/>
               <Route path='/wahlanleitung' element={<WahlHeader/>}/>
-            </Routes>
-          </Switch>
+          </Routes>   
       </div>
       <Routes>
-        <Switch>
           <Route path="" element={<div><Break /><Problem /><Ziele /><Stimmen /></div>}/>
           <Route path="/datenschutz" element={<Datenschutz/>}/>
           <Route path="/impressum" element={<Impressum/>}/>
           <Route path="/wahlanleitung" element={<Wahl/>}/>
-        </Switch>
       </Routes>
       <Footer />
     </div>
