f8e7781fa4fc575497195348f4992db95449078b
Lokoschade <fr.ratteit@gmx.de>
2024-06-12 21:06:46 +0200

    HashRouter statt BrowserRouter

--- src/components/pageNotFound/PageNotFound.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PageNotFound = () => {
-  return <div>Hier ist was schiefgegangen.</div>;
-};
-
-export default PageNotFound;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import App from './App';
 import './index.css';
@@ -9,5 +9,5 @@ const rootElement = document.getElementById('root');
 
 const root = createRoot(rootElement);
 
-root.render(<BrowserRouter basename="/"><App/>
-</BrowserRouter>);
+root.render(<HashRouter><App/>
+</HashRouter>);
