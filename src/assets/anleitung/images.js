399c69ca3b9f86e97b5ba5bd36c972a6a2bff295
Lokoschade <fr.ratteit@gmx.de>
2024-06-17 15:48:20 +0200

    Wahlanleitung hinzugefügt

--- src/assets/anleitung/images.js
+++ src/assets/anleitung/images.js
@@ -1,10 +1,10 @@
-import image1 from './anleitung1.png';
-import image2 from './anleitung2.png';
-import image3 from './anleitung3.png';
-import image4 from './anleitung4.png';
-import image5 from './anleitung5.png';
-import image6 from './anleitung6.png';
-import image7 from './anleitung7.png';
+import image1 from './anleitung1.PNG';
+import image2 from './anleitung2.PNG';
+import image3 from './anleitung3.PNG';
+import image4 from './anleitung4.PNG';
+import image5 from './anleitung5.PNG';
+import image6 from './anleitung6.PNG';
+import image7 from './anleitung7.PNG';
 
 const images = [image1, image2, image3, image4, image5, image6, image7];
 
--- src/components/wahl/Wahl.jsx
+++ src/components/wahl/Wahl.jsx
@@ -1,30 +1,32 @@
 import React from "react";
 import "./wahl.css";
 import images from "../../assets/anleitung/images";
+import { useTranslation } from "react-i18next";
 
 const Wahl = () => {
-  const steps = [
-    { image: images[0], text: "Text für Bild 1" },
-    { image: images[1], text: "Text für Bild 2" },
-    { image: images[2], text: "Text für Bild 3" },
-    { image: images[3], text: "Text für Bild 4" },
-    { image: images[4], text: "Text für Bild 5" },
-    { image: images[5], text: "Text für Bild 6" },
-    { image: images[6], text: "Text für Bild 7" },
-  ];
+  const { t } = useTranslation();
 
   return (
     <div className="jg__wahl">
-      {steps.map((step, index) => (
-        <div key={index} className="jg__wahl-step">
-          <img
-            src={step.image}
-            alt={`Step ${index + 1}`}
-            className="jg__wahl-image"
-          />
-          <p className="jg__wahl-text">{step.text}</p>
-        </div>
-      ))}
+      <p className="jg__wahl-text">{t("wahl.step1")}</p>
+      <img src={images[0]} alt="Step1" className="jg__wahl-image" />
+      <p className="jg__wahl-text">{t("wahl.step2")}</p>
+      <img src={images[1]} alt="Step2" className="jg__wahl-image" />
+      <p className="jg__wahl-text">{t("wahl.step3")}</p>
+      <img src={images[2]} alt="Step3" className="jg__wahl-image" />
+      <p className="jg__wahl-text">{t("wahl.step4")}</p>
+      <p className="jg__wahl-text">{t("wahl.step5_1")}</p>
+      <p className="jg__wahl-text">{t("wahl.step5_2")}</p>
+      <img src={images[3]} alt="Step4" className="jg__wahl-image" />
+      <p className="jg__wahl-text">{t("wahl.step5_21")}</p>
+      <img src={images[4]} alt="Step5" className="jg__wahl-image" />
+      <p className="jg__wahl-text">{t("wahl.step5_22")}</p>
+      <img src={images[5]} alt="Step6" className="jg__wahl-image" />
+      <p className="jg__wahl-text">{t("wahl.step5_23")}</p>
+      <img src={images[6]} alt="Step7" className="jg__wahl-image" />
+      <p className="jg__wahl-text">{t("wahl.step6")}</p>
+      <p className="jg__wahl-text">{t("wahl.step7")}</p>
+      <p className="jg__wahl-text">{t("wahl.hinweis")}</p>
     </div>
   );
 };
